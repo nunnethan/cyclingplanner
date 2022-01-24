5f9f434dba4bb6194af9a12c3c9ed3ba31a2bf91
Ethan Nunn <nunnethan@gmail.com>
2022-01-24 10:51:26 +0000

    Initial Commit

---
+++ Shared/cyclingplannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cyclingplannerApp.swift
+//  Shared
+//
+//  Created by Ethan Nunn on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct cyclingplannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
